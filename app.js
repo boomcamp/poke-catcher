f232855d06f44cf5199c61babf065000deeccde4
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-06 18:04:42 +0800

    finished initial look

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    setTimeout(function() { 
+        $(".left-joycon").fadeIn();
+        $(".right-joycon").fadeIn();
+        $(".screen").fadeIn();
+    }, 500);
+  });
