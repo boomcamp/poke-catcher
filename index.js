73465a7e2bda7fbe58665aeb1510b7c68d2c5968
Jan Patrick L. Bañares <janpatrick.banares@boom.camp>
2019-06-07 16:49:55 +0800

    updated files

---
+++ index.js
@@ -0,0 +1,81 @@
+const regSelect = document.querySelector("#reg");
+const locSelect = document.querySelector("#loc");
+const areaSelect = document.querySelector("#area");
+const fetchAPI = function() {
+  const pokeUrl = "https://pokeapi.co/api/v2";
+  function getReg(path) {
+    return fetch(`${pokeUrl}/${path}`)
+      .then(res => res.json())
+      .then(function(data) {
+        let results = data.results;
+
+        regSelect.innerHTML = results.map(region => {
+          if (results.indexOf(region) == 0) {
+            return `<option value="" selected>Select Region</option>
+            <option value="${region.url}">${region.name}</option>`;
+          } else {
+            return `<option value="${region.url}">${region.name}</option>`;
+          }
+        });
+        regSelect.dispatchEvent(new Event("change"));
+        var locUrl = regSelect.value;
+        regSelect.addEventListener("change", function(e) {
+          locUrl = e.target.value;
+          console.log(e.target.value);
+
+          return fetch(`${locUrl}`)
+            .then(res => res.json())
+            .then(function(data) {
+              let resultsLoc = data.locations;
+              locSelect.innerHTML = resultsLoc.map(location => {
+                if (resultsLoc.indexOf(location) == 0) {
+                  return `<option value="" selected>Select Location</option>
+                <option value="${location.url}">${location.name}</option>`;
+                } else {
+                  return `<option value="${location.url}">${
+                    location.name
+                  }</option>`;
+                }
+              });
+            });
+        });
+        var areaUrl = locSelect.value;
+        locSelect.addEventListener("change", function(e) {
+          areaUrl = e.target.value;
+          console.log(e.target.value);
+
+          return fetch(`${areaUrl}`)
+            .then(res => res.json())
+            .then(function(data) {
+              let resultsArea = data.areas;
+              areaSelect.innerHTML = resultsArea.map(area => {
+                if (resultsArea.indexOf(area) == 0) {
+                  return `<option value="" selected>Select Area</option>
+                <option value="${area.url}">${area.name}</option>
+                        `;
+                } else {
+                  return `<option value="${area.url}">${area.name}</option>`;
+                }
+              });
+              
+            });
+        });
+        var pokeEncounter= areaSelect.value;
+       areaSelect.addEventListener("change", function(e){
+         pokeEncounter = e.target.value;
+         console.log(pokeEncounter)
+
+         return fetch(`${pokeEncounter}`)
+                  .then(res => res.json())
+                  .then(function(data){
+                    //let pokemonRes = data.pokemon-encounter;
+                    console.log(data)
+                  })
+           
+       })
+      });
+  }
+  return getReg("region");
+};
+
+fetchAPI();
