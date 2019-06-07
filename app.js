b7d345918f8b9a8e8da30333013ed2ade6f1fff7
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-07 17:59:24 +0800

    can fetch region data

--- app.js
+++ app.js
@@ -21,7 +21,24 @@ $(document).ready(function(){
         }, 600);
       
     });
-
-    
-    
   });
+
+
+  //fetch
+
+  fetch('https://pokeapi.co/api/v2/region/')
+  .then((response => response.json())) // Call the fetch function passing the url of the API as a parameter
+  .then(function(response) {
+      // Your code for handling the data you get from the API
+      
+      for(x = 0; x <= response.results.length; x++){
+        console.log(response.results[x].name);
+        $('#region-select').append($(`<option>${response.results[x].name}</option>`));
+      }
+      
+  })
+  .catch(function() {
+      // This is where you run code if the server returns any errors
+  });
+  
+  
