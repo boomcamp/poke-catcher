1e89eb19f8f488f2ec4182f34349ebc75a8a4b75
Koji Adriano Jr <rolando.adriano@boom.camp>
2019-06-07 16:23:32 +0800

    design phase 1 done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-const pokeRegions = 'https://pokeapi.co/api/v2/region';
+$(function() {
+    var pageTitle = $("title").text();
+    $(window).blur(function() {
+        $("title").text("Come Back! You gotta catch 'em all!");
+    });
+    $(window).focus(function() {
+        $("title").text(pageTitle);
+    });
+});
 
+
+
+const pokeRegions = 'https://pokeapi.co/api/v2/region';
 const regions = [];
 
 
 fetch(pokeRegions)
     .then(response => response.json())
     .then(regs => {
-        regions.push(regs)
+        regions.push(regs.results)
         console.log(regions)
     })
     .catch(err => {
