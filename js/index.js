936135dd4119e5d381ea2d7bbdaad53f802bcbaa
Samuel A. Lopez <samuel.lopez@boom.camp>
2019-06-07 16:49:46 +0800

    Second Save

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+const regionSelect = document.getElementById('region');
+const locationSelect = document.getElementById('location');
+const areaSelect = document.getElementById('area');
+
+
 const fetchAPI = (path) => {
     const baseURL = `https://pokeapi.co/api/v2/`;
 
@@ -5,9 +10,72 @@ const fetchAPI = (path) => {
         .then(res => res.json())
         .then(function (posts)  {
             var postsValue = posts.results;
+            
+            if(path === 'region'){
+                regionSelect.innerHTML = postsValue.map(function(arrValue){
+                    
+                    if(postsValue.indexOf(arrValue) == 0){
+                         return `
+                         <option value=""  selected ">Select Region</option>
+                         <option value="${arrValue.url}"   ">${arrValue.name}</option>`;
+                    }else{
+                        return `<option value="${arrValue.url} ">${arrValue.name}</option>`;
+                    }  
+                 })
+            }
+
+            region.addEventListener('change', function() {
+                let locationUrl = this.value;
+                //console.log(locationUrl);
+                return fetch(`${locationUrl}`)
+                .then(res => res.json())
+                .then(function (posts)  {
+                    var postsValue = posts.locations;
+
+                    locationSelect.innerHTML = postsValue.map(function(arrValue){
+                        if(postsValue.indexOf(arrValue) == 0){
+                            return `
+                            <option value=""  selected ">Select Region</option>
+                            <option value="${arrValue.url}"   ">${arrValue.name}</option>`;
+                       }else{
+                           return `<option value="${arrValue.url} ">${arrValue.name}</option>`;
+                       } 
+
+                    })
+                })
+            
+            });
+
+            locationSelect.addEventListener('change', function() {
+                let areaURL = this.value;
+                // console.log(areaURL);
+                return fetch(`${areaURL}`)
+                .then(res => res.json())
+                .then(function (posts)  {
+                    var postsValue = posts.areas;
+
+                    areaSelect.innerHTML = postsValue.map(function(arrValue){
+
+                        if(postsValue.indexOf(arrValue) == 0){
+                            return `
+                            <option value=""  selected ">Select Region</option>
+                            <option value="${arrValue.url}"   ">${arrValue.name}</option>`;
+                       }else{
+                           return `<option value="${arrValue.url} ">${arrValue.name}</option>`;
+                       } 
+
+                    })
+                })
+            
+            });
+            
  
-            postsValue.forEach(function(arrValue){
-                console.log(arrValue.name);
-            })
         })
+        .catch(console.error);
+        
 }
+
+fetchAPI('region');
+
+
+
