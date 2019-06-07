31514a400ff4e878477201762ad66859496dcdd4
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-07 15:16:26 +0800

    loading screen overhaul

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
 
     setTimeout(function() { 
-        $(".left-joycon").fadeIn();
-        $(".right-joycon").fadeIn();
-        $(".screen").fadeIn();
+        $('body').animate({
+            opacity: 1,
+        }, 'slow');
+        console.log('1');
     }, 500);
+    setTimeout(function() { 
+        $('.poke-logo').fadeIn();
+        console.log('2');
+    }, 1000);
+ 
+    $('.poke-logo').click(function(){
+       
+        $('.start-screen').fadeOut();
+        setTimeout(function() { 
+            $(".center-top").css('background-image', 'none');
+            $(".center-top").css('background-color', '#fff');
+        }, 500);
+        
+        console.log('pokelogo');
+    });
+
+    
+    
   });
