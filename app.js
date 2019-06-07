d64e7d571f559052c4980f55208cd01e57956c48
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-07 16:43:33 +0800

    added main game grid

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ $(document).ready(function(){
         $('body').animate({
             opacity: 1,
         }, 'slow');
-        console.log('1');
     }, 500);
     setTimeout(function() { 
         $('.poke-logo').fadeIn();
-        console.log('2');
     }, 1000);
  
     $('.poke-logo').click(function(){
-       
         $('.start-screen').fadeOut();
         setTimeout(function() { 
             $(".center-top").css('background-image', 'none');
             $(".center-top").css('background-color', '#fff');
         }, 500);
-        
-        console.log('pokelogo');
+        setTimeout(function() { 
+            $('.start-screen').remove();
+            $( ".main-game" ).removeClass( "hide" ).addClass( "show" );
+        }, 600);
+      
     });
 
     
