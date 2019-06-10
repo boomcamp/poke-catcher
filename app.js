776cd789e9076ef7928b8e12f1fa0e4cd202c6ed
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-10 12:00:17 +0800

    added regions and city dropdown

--- app.js
+++ app.js
@@ -19,26 +19,83 @@ $(document).ready(function(){
             $('.start-screen').remove();
             $( ".main-game" ).removeClass( "hide" ).addClass( "show" );
         }, 600);
-      
+    
     });
   });
 
 
   //fetch
-
+  var arrRegions = [];
   fetch('https://pokeapi.co/api/v2/region/')
-  .then((response => response.json())) // Call the fetch function passing the url of the API as a parameter
-  .then(function(response) {
-      // Your code for handling the data you get from the API
-      
-      for(x = 0; x <= response.results.length; x++){
-        console.log(response.results[x].name);
-        $('#region-select').append($(`<option>${response.results[x].name}</option>`));
-      }
-      
-  })
-  .catch(function() {
-      // This is where you run code if the server returns any errors
-  });
+    .then((response => response.json())) 
+    .then((response) => {
+        console.log('1');
+        return response.results
+        })
+    .then((response) => { 
+        // add all the regions dropdown
+        console.log(response);
+        response.forEach(element => {
+            $('#region-select').append(`
+            <option>
+            ${element.name}
+            </option>
+            `);
+            });
+        return response
+    })
+    .then((response) => { // on region name get url for the city dropdown
+        $('#region-select').on('change', function() {
+            console.log(this.value)
+            var regionName = this.value;
+            response.forEach(element => {
+                if (regionName == element.name){
+                   fetch(element.url)
+                   .then((response => response.json()))
+                   .then((response => response.locations))  
+                   .then((response) => { 
+                    // add all the cities dropdown
+                    console.log(response);
+                    response.forEach(element => {
+                        $('#city-select').append(`
+                        <option>
+                        ${element.name}
+                        </option>
+                        `);
+                        });
+                    return response
+                    })
+                    .catch(function(err) {
+                    console.error(err);
+            });
+                
+                }
+            });      
+        });
+    })
+    .then((url) => console.log(url) )
+    .catch(function(err) {
+        console.error(err);
+});
   
+
+
+
+
+// .then((response) => { // on region name get url for the city dropdown
+//     $('#region-select').on('change', function() {
+//         console.log(this.value)
+//         var regionName = this.value;
+//         response.forEach(element => {
+//             if (regionName == element.name){
+//                 $('#city-select').append(`
+//                 <option>
+//                 ${element.url}
+//                 </option>
+//                 `);
+//                 console.log('ye')
+//             }
+//         });      
+//     });
+// })
   
