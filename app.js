49390651c44aba2189372a313fbe064826df3d3f
EJBarba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-06-10 15:38:22 +0800

    changed location logo and added area dropdown

--- app.js
+++ app.js
@@ -45,35 +45,87 @@ $(document).ready(function(){
         return response
     })
     .then((response) => { // on region name get url for the city dropdown
+        var regionUrl; 
         $('#region-select').on('change', function() {
-            console.log(this.value)
+            var getUrl; // passes it outside of scope
             var regionName = this.value;
             response.forEach(element => {
-                if (regionName == element.name){
+                if (regionName == element.name){ 
                    fetch(element.url)
                    .then((response => response.json()))
                    .then((response => response.locations))  
                    .then((response) => { 
                     // add all the cities dropdown
+                    console.log('locations in region');
                     console.log(response);
+
+                    $('#city-select').find('option').remove(); //cleans the city dropdown for previous populations  //fixes bug where selecting regions keeps adding the cities
                     response.forEach(element => {
                         $('#city-select').append(`
                         <option>
                         ${element.name}
                         </option>
                         `);
-                        });
-                    return response
+                       
+                        });                 
                     })
                     .catch(function(err) {
                     console.error(err);
-            });
-                
+                    });
+                    
+                    var getTheUrl = element.url;
+                    console.log('getTheUrl ' + getTheUrl); 
+
+                getUrl = getTheUrl; // passes it outside of scope
+                console.log('getURL' +getUrl); //debugger    
                 }
-            });      
+                
+            });  
+      
+        regionUrl = getUrl; // gets the region url from inner scopes
+        console.log(regionUrl + 'region url');
         });
+        
+        $('#city-select').on('change', function (){
+            fetch(regionUrl)
+            .then((response => response.json()))
+            .then((response) => response.locations)
+            .then((response) => {
+                response.forEach(element =>{
+                    //gets areas from url of city 
+                    if (this.value == element.name){
+                        fetch(element.url)
+                        .then((response => response.json()))
+                        .then(response => response.areas)
+                        .then(response => {
+                            $('#area-select').find('option').remove(); //cleans the city dropdown for previous populations  //fixes bug where selecting regions keeps adding the cities
+                            response.forEach(element => {
+                                $('#area-select').append(`
+                                <option>
+                                ${element.name}
+                                </option>
+                                `);
+                               
+                                }); 
+                        })
+                        .catch(function(err) {
+                            console.error(err);
+                            });
+                    }  
+                   
+                }) 
+
+            })
+
+            .catch(function(err) {
+                console.error(err);
+            }); 
+            
+           
+        })
     })
-    .then((url) => console.log(url) )
+    
+  
     .catch(function(err) {
         console.error(err);
 });
@@ -82,20 +134,3 @@ $(document).ready(function(){
 
 
 
-// .then((response) => { // on region name get url for the city dropdown
-//     $('#region-select').on('change', function() {
-//         console.log(this.value)
-//         var regionName = this.value;
-//         response.forEach(element => {
-//             if (regionName == element.name){
-//                 $('#city-select').append(`
-//                 <option>
-//                 ${element.url}
-//                 </option>
-//                 `);
-//                 console.log('ye')
-//             }
-//         });      
-//     });
-// })
-  
