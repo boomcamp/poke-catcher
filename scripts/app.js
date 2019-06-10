f2057b7289403a0c1b21d3ad428e8ebc9daf9207
judesama <judeagagad@gmail.com>
2019-06-10 14:05:52 +0800

    Explore

--- scripts/app.js
+++ scripts/app.js
@@ -13,36 +13,48 @@ function jsUcfirst(string)
 
 
 $(document).ready(function(){
-
+    $('.capture-btn').hide();
+  
     
+
     $('.regions-container').hide();
     $('.locations-container').hide();
     $('.game-container').hide();
-    $('.magnify').hide();
-
+ 
 
     $('.landing-title').click(function(){
         $('.landing-container').fadeOut();
         $('.regions-container').fadeIn();
+        clearStat();
+        
     })
 
    
     $('.backtoMain').click(function(){
         $('.regions-container').fadeOut();
         $('.landing-container').fadeIn();
+        clearStat();
+        
     })
 
     $('.backtoRegion').click(function(){
         $('.locations-container').fadeOut();
         $('.region-display').fadeIn();
+        clearStat();
+
     })
     
     $('.backtoLocation').click(function(){
+        clearStat();
         $('.game-container').fadeOut();
-        $('.regions-container').fadeIn();
-        document.querySelector('body').className = '';
 
+        $('.regions-container').fadeIn();
+
+        document.querySelector('body').className = '';
+      
+        
     })
+
 
    getRegions('https://pokeapi.co/api/v2/region')
     .then(function(regions){
@@ -65,7 +77,7 @@ $(document).ready(function(){
                
                 $('.region-display').fadeOut();
                 $('.locations-container').fadeIn();
-                
+                $('.stats-wrapper').empty();
                 viewLocation(regionURL);
 
             })
@@ -76,36 +88,72 @@ $(document).ready(function(){
 
 })
 
-function viewPokemon(url){
+
+function clearStat(){
+    $('.stats-wrapper').html('');
+
+}
+
+
+
+function viewPokemon(url , baseUrl ,areaName){
+
+   
     return fetch(url)
         .then(function(response){
             return response.json();
         })
         .then(function(pokemon){
-            let randTime = getRandomArbitrary(3000 , 10000)
+           
             let pokemonImgSrc = pokemon.sprites.front_default;
-            
-            $('.magnify').fadeIn();
-            $('.explore-btn-wrapper').fadeOut();
-            $('.pokemon-display').fadeOut();
+            let randTime = getRandomArbitrary(3000 , 5000)
+           
+            pokemonStats = pokemon.stats;
+            let imgDisplay = document.querySelector('.img-display');
+            imgDisplay.src = 'img/lg.searching-for-loading-icon.gif';
+           
+            $('.explore-btn').fadeOut();
+            $('.text-display').empty();
+            $('.capture-btn').fadeOut();
+            $('.stats-wrapper').empty();
+
+            $('.stat-title').html('Searching for Pokemon')
             setTimeout(function(){
-              
-                $('.pokemon-display').fadeIn();
-                $('.magnify').fadeOut();
-                $('.explore-btn-wrapper').fadeIn();
-
-                let pokemonImg = $(
-                    `
-                    <img src="${pokemonImgSrc}" width="200">
-                     
-                    `
-                )
-
-                $('.pokemon-display').html(pokemonImg);
+                
+                
+                $('.capture-btn').fadeIn();
+               
+                $('.explore-btn').fadeIn();
+                imgDisplay.src = '';
+                imgDisplay.src = `${pokemonImgSrc}`;
+                console.log(randTime , pokemonImgSrc);  
+            
+                $('.stats-wrapper').empty();
 
 
+                $('.text-display').html(`You found a ${pokemon.name}`);
+                pokemonStats.forEach(element => {
+                    let stats = element.stat;
+                    let displayStats = $(`
+                        <div class="stat">
+                            <span>${stats.name}</span>
+                            <span class="float-right">${element.base_stat}</span>
+                        </div>
+                    `);
+
+                    $('.stats-wrapper').append(displayStats);
+                    $('.stat-title').html('Statistics')
+
+                    
+                });
              
             } , randTime)
+            
+            $('.capture-btn').click(function(){
+                console.log(pokemon);
+            })
+            
+            
 
         })
       
@@ -113,10 +161,12 @@ function viewPokemon(url){
 
 
 }
-
+function capture(pokemonName , imgSrc){
+    console.log('CAPTURED!');
+}
 
 function explore(url , areaName){
- 
+    
     $(`.regions-container`).fadeOut();
     $(`.game-container`).fadeIn();
     $(`.area-name`).html(areaName);
@@ -132,14 +182,16 @@ function explore(url , areaName){
         })
         .then(function(area){
             let pokemonEncounters = area.pokemon_encounters;
-
+           
             $('.explore-btn').click(function(){
-                let randNum = getRandomArbitrary(0 , pokemonEncounters.length);
-
+                let randomNum = 0;
+            
+                randNum = getRandomArbitrary(0 , pokemonEncounters.length);
                 // setTimeout(function(){
                 //     console.log(pokemonEncounters[randNum]);
                 // } , randTime)
-                viewPokemon(pokemonEncounters[randomNum].pokemon.url)
+                console.log(randNum);
+                viewPokemon(pokemonEncounters[randNum].pokemon.url , url , areaName)
             })
 
 
@@ -172,9 +224,7 @@ function viewArea(url){
 
                    
                    $(`.${element.name}-pokeArea`).click(function(){
-                       
                         explore(element.url , location.name);
-                        
                    })
 
 
@@ -185,7 +235,7 @@ function viewArea(url){
                 let noArea = $(
                     `<li class="list-group-item-area">No available area</li>`
                 )
-
+                
                 $(`.${locationName}-area`).html(noArea);
 
             }
@@ -230,7 +280,7 @@ function viewLocation(region){
 
                 $(`.${element.name}-location`).click(function(){
                     
-                 
+                    $('.stats-wrapper').empty();
                     $(`.${element.name}-area`).toggle();
                 })
             });
