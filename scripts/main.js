44b88ea826c9e7faa876c383a2587fb701e096be
Micko Matamorosa <micko.matamorosa@boom.camp>
2019-10-31 17:49:36 +0800

    Almost done

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,20 @@
-
-
-
 (function region(){
   fetch('https://pokeapi.co/api/v2/region')
   .then((res) => res.json())
   .then((data)=> {
-    let output='';
+    let output='<option disabled selected>Choose region</option>';
     data.results.forEach(region => {
       output += `
         <option value="${region.url}">${region.name}</option>
         `;
     });
     document.getElementById('region').innerHTML = output;
-
+   
+    
+  })
+  document.getElementById('region').addEventListener('change', function(){
+    document.getElementById('location').removeAttribute('disabled');
+    document.getElementById('area').innerHTML = `<option disabled selected>Choose area</option>`;
   })
 })()
 
@@ -21,7 +23,7 @@ document.getElementById('region').addEventListener('change', function(){
   fetch(`${document.getElementById('region').value}`)
   .then((res) => res.json())
   .then((data) => {
-    let output='';
+    let output='<option disabled selected>Choose location</option>';
     data.locations.forEach(location =>{
       output += `
       <option value="${location.url}">${location.name}</option>
@@ -29,6 +31,7 @@ document.getElementById('region').addEventListener('change', function(){
     });
     document.getElementById('location').innerHTML = output;
   })
+  
 })
 
 
@@ -37,39 +40,80 @@ document.getElementById('location').addEventListener('change', function(){
   .then((res) => res.json())
   .then((data) => {
     // console.log(data.areas);
-    let output='';
+    let output='<option disabled selected>Choose area</option>';
     data.areas.forEach(area =>{
       output += `
       <option value="${area.url}">${area.name}</option>
       `;
     });
+    document.getElementById('area').removeAttribute('disabled');
     document.getElementById('area').innerHTML = output;
   })
 })
 
-
+let counter = 0;
 document.getElementById('explore-btn').addEventListener('click', function(){
-  fetch(`${document.getElementById('area').value}`)
+  fetch(`${document.getElementById('area').value}`) //FETCH POKEMON BY AREA
   .then((res) => res.json())
   .then((data) => {
-    // console.log(data.pokemon_encounters[0]);
-    // let output='';
-    // data.pokemon_encounters.forEach(poke_en =>{
-      
-    //     output += `${poke_en.pokemon.url}`;
-    //     fetch
-    // });
+  
     var encounters = data.pokemon_encounters;
-    var randomNumber = Math.floor(Math.random()*(data.pokemon_encounters.length));
+    var randomNumber = Math.floor(Math.random()*(encounters.length));
     var pokemonName = encounters[randomNumber].pokemon.name;
     var pokemonUrl = encounters[randomNumber].pokemon.url;
-    console.log(pokemonUrl);
     
-    document.getElementById('laogan').innerHTML = `
-    <img src="images/poke-logo.jpg" alt="" />
+    fetch(`${pokemonUrl}`) //FETCH POKEMON CHARACTERISTICS
+    .then((nres) => nres.json())
+    .then((ndata) => {
+
+      var logoUrl = ndata.sprites.front_default;
+      
+      document.getElementById('laogan').innerHTML = `
+      <img src="${logoUrl}" alt="" />
           <h2>${pokemonName}</h2>
           <button class="capture-btn" id="capture">Capture!</button>
-    `;
+      `;
+      // STATS
+      let output =`
+      <h3>Details:</h3>
+      <ul>`;
+      ndata.stats.forEach(statt =>{
+        let baseStat = statt.base_stat;
+        let statName = statt.stat.name;
+        
+      output += `
+        <li>${statName}: ${baseStat}</li>
+      `;
+      });
+      output += '</ul>';
+      document.getElementById('detalye').innerHTML = output;
+
+
+      //ON CAPTURE
+      $('#capture').on('click', function(){
+        document.getElementById('caps').innerText=`${counter+1}/6`;
+        let laogan = document.getElementById('laogan');
+        let tag = laogan.getElementsByTagName('img')[0];
+        let pokePicUrl = tag.src;
+        
+        let pokeName = laogan.getElementsByTagName('h2').innerHTML;
+        if(counter != 6){
+            ++counter;
+
+        document.getElementById(`${counter}`).innerHTML =`
+        <img src="${pokePicUrl}" alt="" />
+        <h2>${pokeName}</h2>
+        `;
+        }
+
+      })
+
+
+    })
 
   })
+
+  // var audio = new Audio('../sounds/pokebattle.mp3');
+  // audio.currentTime = 0;
+  // audio.play();
 })
